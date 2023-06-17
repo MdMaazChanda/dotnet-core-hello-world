f7b918afeed52c8dab67d4d9a9ad573a22bc1d45
MdMaazChanda <78657758+MdMaazChanda@users.noreply.github.com>
2023-06-17 12:21:14 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,26 +1,12 @@
-using System.IO;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using System;
 
-namespace HelloWeb
+namespace DotNet.Docker
 {
-    public class Program
+    class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-
-            var config = new ConfigurationBuilder()
-                          .AddCommandLine(args)
-                          .Build();
-            var host = new WebHostBuilder()
-                        .UseKestrel()
-                        .UseConfiguration(config)
-                        .UseContentRoot(Directory.GetCurrentDirectory())
-                        .UseIISIntegration()
-                        .UseStartup<Startup>()
-                        .Build();
-
-            host.Run();
+            Console.WriteLine("Hello World!");
         }
     }
 }
